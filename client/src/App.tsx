558daa6b5eaabf763c14114f9617f0df5abe6367
Tzakopoulos Panagiotis <115104147+ptzakopoulos@users.noreply.github.com>
2026-02-21 11:19:11 +0200

    React Router Integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
+import "./css/generals.scss";
 import AddProject from "./pages/add-project/add-project";
+import MasterLayout from "./layouts/master.layout";
+import Home from "./pages/home/home";
+import MyProjects from "./pages/home/my-projects/my-projects";
+import MyColleagues from "./pages/home/my-colleagues/my-colleagues";
+import SingleProject from "./pages/single-project/single-project";
+import NotFound from "./pages/404/404";
+import MyClients from "./pages/home/my-clients/my-clients";
 
 function App() {
   return (
-    <>
-      <AddProject></AddProject>
-    </>
+    <Routes>
+      <Route element={<MasterLayout />}>
+        <Route path="/" element={<Home />}>
+          <Route index element={<MyProjects />} />
+          <Route path="/my-projects" element={<MyProjects />} />
+          <Route path="/my-colleagues" element={<MyColleagues />} />
+          <Route path="/my-clients" element={<MyClients />} />
+        </Route>
+        <Route path="/new-project" element={<AddProject />} />
+        <Route path="/project/:id" element={<SingleProject />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/layouts/master.layout.tsx
@@ -0,0 +1,43 @@
+import { Outlet, Link } from "react-router";
+import STYLES from "./style.module.scss";
+export default function MasterLayout() {
+  return (
+    <div className={STYLES.appContainer}>
+      <header className={STYLES.header}>
+        <p>Navigation</p>
+        <br />
+        <nav>
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/new-project"}>New Project</Link>
+            </li>
+            <li>
+              <Link to={"/logs"}>Logs</Link>
+            </li>
+          </ul>
+        </nav>
+        <br />
+        <br />
+        <br />
+        <p>Settings</p>
+        <br />
+        <nav>
+          <ul>
+            <li>
+              <Link to={"/profile"}>Profile</Link>
+            </li>
+            <li>
+              <Link to={"/preferences"}>Preferences</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className={STYLES.wrapper}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
