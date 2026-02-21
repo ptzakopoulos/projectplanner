cd3ce4c17eac1b4380928159883af6dc5b1e3feb
Tzakopoulos Panagiotis <115104147+ptzakopoulos@users.noreply.github.com>
2026-02-21 11:18:23 +0200

    Models integration

---
+++ client/src/types/models.ts
@@ -0,0 +1,31 @@
+type NewProjectLink = {
+  name: string;
+  url: string;
+};
+type NewProjectColleague = {
+  role: string;
+  name: string;
+  email: string;
+};
+type NewProjectDomain = {
+  name: string;
+  url: string;
+  username: string;
+  password: string;
+};
+type NewProjectClient = {
+  icon?: string;
+  name: string;
+  email?: string;
+};
+type NewProject = {
+  title: string;
+  description?: string;
+  links?: NewProjectLink[];
+  colleagues?: NewProjectColleague[];
+  domains?: NewProjectDomain[];
+  clients?: NewProjectClient[];
+  deadline?: Date;
+};
+
+export type { NewProject };
