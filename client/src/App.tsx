43ee5d2d9f9f2bd273741de6f0a1770e229057c6
Tzakopoulos Panagiotis <115104147+ptzakopoulos@users.noreply.github.com>
2026-03-12 18:16:56 +0200

    App.tsx Routes Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,13 +10,29 @@ import SingleProject from "./pages/single-project/single-project";
 import NotFound from "./pages/404/404";
 import MyClients from "./pages/home/my-clients/my-clients";
 import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import Logout from "./pages/logout/logout";
+import { useAuthContext } from "./hooks/useAuthContext";
 function App() {
+  const { token } = useAuthContext();
+
+  if (!token)
+    return (
+      <Routes>
+        <Route element={<MasterLayout token={token} />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    );
+
   return (
     <Routes>
-      <Route element={<MasterLayout />}>
+      <Route element={<MasterLayout token={token} />}>
         <Route path="/" element={<Home />}>
           <Route index element={<MyProjects />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/my-projects" element={<MyProjects />} />
           <Route path="/my-colleagues" element={<MyColleagues />} />
           <Route path="/my-clients" element={<MyClients />} />
@@ -24,6 +40,7 @@ function App() {
         <Route path="/new-project" element={<AddProject />} />
         <Route path="/project/:projectId" element={<SingleProject />} />
         <Route path="/project/edit/:projectId" element={<AddProject />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
